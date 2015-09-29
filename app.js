ebc79281c9544a2b20f3a875688340293e1bec94
alexandra brown & ben casalino <student@epicodus.com>
2015-09-29 10:49:01 -0700

    add more stuff to the thing

---
+++ app.js
@@ -0,0 +1,30 @@
+var Freelancerer = angular.module("Freelancerer", ["ui.router"]);
+Freelancerer.config(function($stateProvider, $urlRouterProvider)
+
+ {
+  $stateProvider.state("home", {
+    url: "",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
+});
+
+
+
+$stateProvider.state("slideshow", {
+  url: "",
+  views: {
+    'header': {
+      templateUrl: 'partials/header.html',
+    },
+    'body': {
+      templateUrl: "partials/slideshow.html",
+    },
+  }
+});
+});
